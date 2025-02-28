a7bc8bfe77cdc03184f94e378a55187bb5eb76fe
Pedro Mota <116939549+PMota173@users.noreply.github.com>
2025-02-28 13:32:17 -0300

    add: função imprimir a cache

--- cache_simulator.py
+++ cache_simulator.py
@@ -1,18 +1,17 @@
-# Linha de comando simplificada:
-# cache_simulator <nsets> <bsize> <assoc> <subst> <flag_saida> arquivo_de_entrada.bin
-
 
 import sys
 import math
 
 def main():
+    # Linha de comando simplificada:
+    # cache_simulator <nsets> <bsize> <assoc> <subst> <flag_saida> arquivo_de_entrada.bin
 
     # possiveis erros de parametros
     if len(sys.argv) != 7:
         print("Parametros invalidos, formato correto: ") 
         print("python cache_simulator <nsets> <bsize> <assoc> <subst> <flag_saida> arquivo_de_entrada.bin")
         sys.exit(1)
-    if sys.argv[4] != "L" and sys.argv[4] != "R" and sys.argv[4] != "F":
+    if (sys.argv[4] != "L" and sys.argv[4] != "l") and (sys.argv[4] != "R" and sys.argv[4] != "r") and (sys.argv[4] != "F" and sys.argv[4] != "f"):
         print("Parametro subst invalido, deve ser L, R ou F (lru, random ou fifo)")
         sys.exit(1)
     if sys.argv[5] != "0" and sys.argv[5] != "1":
@@ -39,7 +38,7 @@ def main():
     print("flag_saida: ", flag_saida)
     print("arquivo: ", arquivo)
 
-
+    # Inicializa a cache
     cache_val = [[0] * assoc for _ in range(nsets)]  # array com o bit de validade de cada bloco
     cache_tag = [[-1] * assoc for _ in range(nsets)]  # array com a tag de cada bloco
 
@@ -48,19 +47,27 @@ def main():
 
     n_bits_tag = 32 - n_bit_offset - n_bit_indice
 
+
     print("n_bit_offset: ", n_bit_offset)
     print("n_bit_indice: ", n_bit_indice)
     print("n_bits_tag: ", n_bits_tag)
 
-    print ("bits de validade: ", cache_val)
-    print ("tags", cache_tag)
+    pretty_print_cache(cache_val, cache_tag)
 
-
-    # flag de saida 1: dados brutos
-    #   ordem: Total de acessos, Taxa de hit, Taxa de miss, Taxa de miss compulsório, 
-    #          Taxa de miss de capacidade, Taxa de miss de conflito
+    # Leitura do arquivo
     
-    # flag de saida 0: dados formatados com labels e valores
+
+def pretty_print_cache(cache_val, cache_tag):
+    print("Cache:")
+    for i in range(len(cache_val)):
+        print("========== Linha ", i, "==========")
+        for j in range(len(cache_val[i])):
+            print("Bloco ", j, end=" ")
+            print("Validade: ", cache_val[i][j], end=" ")
+            print("Tag: ", cache_tag[i][j])
+
+    return
+
 
 if __name__ == "__main__":
     main()
